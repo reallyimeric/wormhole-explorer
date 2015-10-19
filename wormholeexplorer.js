6fd99705c814d3dadecef726bc15171932825a80
Eric Little <reallyimeric@gmail.com>
2015-10-19 12:24:09 +0800

    unxxxxed refrence "qurey"

--- wormholeexplorer.js
+++ wormholeexplorer.js
@@ -8,9 +8,10 @@ if (xhr==null) {
 }
 var url="./wormholeexplorer.php"
 var param
-var id
-id=document.getElementById("id").value
-param="id="+id
+var key,value
+key="parent"
+value=document.getElementById("testinput").value
+param="key="+key+"&"+"value="+value
 xhr.open("POST",url,true)
 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhr.setRequestHeader("Content-length", param.length);
@@ -21,8 +22,9 @@ xhr.send(param)
 
 function display(){
 	if (xhr.readyState==4||xhr.status==200){
-		var result_json=JSON.parse(xhr.responseText)
-		var result=result_json.id+' '+result_json.parent
+		var result=JSON.parse(xhr.responseText)
+		//result='id='+result.id+' p='+result.parent
+		//result='id='+result.id[0]+' p='+result.parent[0]
 		document.getElementById("display_area").innerHTML=result
 	}
 }
--- wormholeexplorer.php
+++ wormholeexplorer.php
@@ -5,15 +5,23 @@
 	define("DB_NAME", "wormholeexplorer");
 	define("TABLE_NAME", "routing");
 
-//	$querymethod=$_POST["querymethod"];
-//	$querycontent=$_POST["querycontent"];
-	$id=$_POST["id"];
+	$key=$_POST["key"];
+	$value=$_POST["value"];
 
 	$dsn = 'mysql:unix_socket='.MYSQL_SOCKET_ADDR.';dbname='.DB_NAME;
 	$dbh = new PDO($dsn, MYSQL_USER, MYSQL_PASSWORD);
-	$sql = 'SELECT * FROM '.TABLE_NAME.' WHERE id = :id';
+    switch ($key)
+    {
+        case "id":
+        break;
+        case "parent":
+        break;
+        default:
+        return;
+    }
+	$sql = 'SELECT * FROM '.TABLE_NAME.' WHERE '.$key.' = :value';
 	$sth = $dbh->prepare($sql);
-	$inputparam = array('id' => $id);
+	$inputparam = array('value'	=>	$value);
 	if( $sth->execute($inputparam) ){
 		$result = $sth->fetch(PDO::FETCH_ASSOC);
 		//print_r($result);
@@ -26,28 +34,30 @@
 		$error = $sth->errorInfo();
 		throw new Exception(json_encode($error));
 		}
-//	$options = array(
-//	    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-//	); 
-//	$dbh = new PDO($dsn, $username, $password, $options);
+/*
+	$options = array(
+	    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	); 
+	$dbh = new PDO($dsn, $username, $password, $options);
 
-//	$dbh = new PDO($dsn, $username, $password);
+	$dbh = new PDO($dsn, $username, $password);
 
-//	$con = mysql_connect(MYSQL_ADDR, MYSQL_USER, MYSQL_PASSWORD);
-//
-//	if (!$con){
-//		die('Could not connect: ' . mysql_error());
-//	}
-//	mysql_select_db(DB_NAME, $con);
-//	if ($querymethod=="show") {
-//		$sql="SELECT * FROM ".$TABLE_NAME." WHERE id = '".$querycontent."'";
-//	}
-//	$result = mysql_query($sql);
-//	print_r($result);
+	$con = mysql_connect(MYSQL_ADDR, MYSQL_USER, MYSQL_PASSWORD);
 
-//	$sth = $dbh->prepare('SELECT *
-//		FROM :table_name
-//		WHERE id = :id');
-//	$table_name=TABLE_NAME;
-//	$sth->execute(array(':table_name' => $table_name, ':id' => $id));
+	if (!$con){
+		die('Could not connect: ' . mysql_error());
+	}
+	mysql_select_db(DB_NAME, $con);
+	if ($querymethod=="show") {
+		$sql="SELECT * FROM ".$TABLE_NAME." WHERE id = '".$querycontent."'";
+	}
+	$result = mysql_query($sql);
+	print_r($result);
+
+	$sth = $dbh->prepare('SELECT *
+		FROM :table_name
+		WHERE id = :id');
+	$table_name=TABLE_NAME;
+	$sth->execute(array(':table_name' => $table_name, ':id' => $id));
+ */
 ?>
