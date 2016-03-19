ca8e9f5254fabb0ed30bce12f761697b67843f6e
Eric <reallyimeric@gmail.com>
2016-03-19 18:59:15 +0800

    correct superglobal variable name, "ENV" to "_ENV"

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <?php
 	//$MYSQL_ADDR = 'unix_socket='.$_ENV["/path/to/socket"].';';
-	$MYSQL_ADDR = 'host='.$_ENV["MYSQL_PORT_3306_TCP_ADDR"].';'.'port='.$ENV["MYSQL_PORT_3306_TCP_PORT"].';';
+	$MYSQL_ADDR = 'host='.$_ENV["MYSQL_PORT_3306_TCP_ADDR"].';'.'port='.$_ENV["MYSQL_PORT_3306_TCP_PORT"].';';
 	$DB_NAME = $_ENV["MYSQL_INSTANCE_NAME"];
 	$TABLE_NAME = "routing";
 	$MYSQL_USER = $_ENV["MYSQL_USERNAME"];
