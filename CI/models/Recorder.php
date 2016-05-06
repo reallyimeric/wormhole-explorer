c6bd9df7e0eda41c8438d73ae649aed50fcd2cdb
Eric Little <reallyimeric@gmail.com>
2016-05-06 16:11:53 +0800

    variable name "allowed" corrected to "params_are_vaild", and POST/DELETE will got corresponding new response now

--- CI/models/Recorder.php
+++ CI/models/Recorder.php
@@ -50,14 +50,17 @@ class Recorder extends CI_Model {
     {
         $p1 = mb_convert_case($p1, MB_CASE_UPPER);
         $p2 = mb_convert_case($p2, MB_CASE_UPPER);
-        $allowed = self::issolarsystem($p1)&&self::issolarsystem($p2);
-        if (!$allowed) {
+        $params_are_vaild = self::issolarsystem($p1)&&self::issolarsystem($p2);
+        if (!$params_are_vaild) {
             return -2;
         }
         $query = $this->db->from("routing")->set('parent', $p1)->set('child', $p2)
         //->get_compiled_insert();
         ->insert();
-        return $query;
+        $query = $this->db->select("*")->from("routing")->where("parent", $p1)->where("child", $p2)
+        ->get();
+        //return $query;
+        return json_encode($query->result());
     }
     public function deleterecord($p1,$p2)       //i'm like getrecord()
     {
@@ -87,9 +90,12 @@ class Recorder extends CI_Model {
         if (!$params_are_vaild) {
             return -2;
         }
+        $selectquery = $this->db->select('*')->from('routing')->where($p1, $p2)
+        ->get();
         $query = $this->db->from("routing")->where($p1, $p2)
         //->get_compiled_delete();
         ->delete();
-        return $query;
+        //return $query;
+        return json_encode($selectquery->result());
     }
 }
