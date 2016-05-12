894aa451e6ed4d3732c6ae42fe777c444b1fe3b1
Eric <reallyimeric@gmail.com>
2016-05-13 04:05:58 +0800

    Update about.php

--- CI/views/about.php
+++ CI/views/about.php
@@ -5,11 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div>
             <?php
                 echo '<p><a href="Record">Routing</a></p>';
-                echo '<p><a href="Welcome">ciwelcome</a></p>';
             ?>
         </div>
         <p>Thank you for using Wormhole Explorer!</p>
         <p>Now using CodeIgniter.</p>
         <p>If you have any idea about this system, feel free to contact <code>reallyimeric@gmail.com</code>, thank you!</p>
-        <p>This page is modified from codeigniter welcome.</p>
+        <p>This page is modified from <a href="Welcome">ci's welcome</a>.</p>
     </div>
